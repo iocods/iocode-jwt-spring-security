1218a172c120b91c66f7601f1d78b0d73326a014
= <=>
2024-06-11 23:12:00 +0100

    Updated the pom xml file to use the latest version of the jwt lib dependency

--- src/main/java/iocode/web/app/service/JwtService.java
+++ src/main/java/iocode/web/app/service/JwtService.java
@@ -2,13 +2,12 @@ package iocode.web.app.service;
 
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
-import java.security.Key;
+import javax.crypto.SecretKey;
 import java.util.Date;
 
 @Service
@@ -17,25 +16,29 @@ public class JwtService {
   @Value("${jwt.secret}")
   private String secret;
 
-  private Key generateSigningKey() {
+  private SecretKey generateSigningKey() {
     return Keys.hmacShaKeyFor(Decoders.BASE64.decode(secret));
   }
 
   public String generateJwtToken(String username) {
+    Date issuedAt = new Date(System.currentTimeMillis());
+    Date expirationDate = new Date(System.currentTimeMillis() + (1000 * 24 *24));
     return Jwts.builder()
-        .setSubject(username)
-        .setIssuedAt(new Date(System.currentTimeMillis()))
-        .setExpiration(new Date(System.currentTimeMillis() + (1000 * 24 *24)))
-        .signWith(generateSigningKey(), SignatureAlgorithm.HS256)
+        .header().type("JWT")
+        .and()
+        .subject(username)
+        .issuedAt(issuedAt)
+        .expiration(expirationDate)
+        .signWith(generateSigningKey())
         .compact();
   }
 
   public Claims extractClaims(String jwtToken) {
-    return Jwts.parserBuilder()
-        .setSigningKey(generateSigningKey())
+    return Jwts.parser()
+        .verifyWith(generateSigningKey())
         .build()
-        .parseClaimsJws(jwtToken)
-        .getBody();
+        .parseSignedClaims(jwtToken)
+        .getPayload();
   }
 
   public String extractSubject(String jwtToken) {
